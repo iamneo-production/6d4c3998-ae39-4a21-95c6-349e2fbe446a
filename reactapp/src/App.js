d2e7221f0b4d90824931353a89a0d8950da5ffda
Neelima1209 <vayugandlaneelima12@gmail.com>
2023-05-09 08:30:15 +0000

    Navbar added to the apply loan page

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React from 'react';
+import {Navigate,BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import ApplyForm from './components/Customer/ApplyForm/ApplyForm';
+import LoanStatus from './components/Customer/LoanStatus/LoanStatus';
+import Profile from './components/Customer/Profile/Profile';
+import Navigationbar from './components/Customer/Navigationbar/Navigationbar';
+
 
 
 function App() {
-  
   return (
-    <div className="App">
-       <ApplyForm/>
-    </div>
+    <>  
+    
+    <Router>
+    <switch>
+      <Navigationbar/>
+        <Routes>
+          <Route exact path="/" element={<Navigate to="ApplyForm"/>}/> 
+          <Route exact path="/ApplyForm" element={<ApplyForm/>}/>
+          <Route path="/LoanStatus" element={<LoanStatus/>} />
+          <Route path="/Profile" element={<Profile/>}/>
+        </Routes>
+        </switch>
+      </Router>
+   
+   </>
     
   );
 }
 
 export default App;
-
--- reactapp/src/components/Customer/ApplyForm/ApplyForm.jsx
+++ reactapp/src/components/Customer/ApplyForm/ApplyForm.jsx
@@ -1,102 +1,97 @@
-import React, { useState } from "react";
+import React from "react";
 import './ApplyForm.css'
 
 
-function ApplyForm() {
-  const [counter, setCounter] = useState(0);
 
-  function handleNextClick() {
-    console.log("Next");
-    setCounter(counter + 1);
-  }
-   
-  
+function ApplyForm() {
+
   return (
-      <div className="apply-form">
-        <form className="form1">
-          <div className="form-group">
-            <input className="input-form"
-              type="text"
-              name="Applicant Name"
-              id="enterName"
-              placeholder="Enter Applicant Name"
-            />
-          </div>
-          <div className="form-group">
-            <input className="input-form"
-              type="text"
-              name="address"
-              id="enterAddress"
-              placeholder="Enter Applicant Address"
-            />
-          </div>
-          
-          <div className="form-group">
-            <input className="input-form"
-              type="text"
-              name="mobileNumber"
-              id="enterMobile"
-              placeholder="Enter Applicant Mobile"
-            />
-          </div>
-          <div className="form-group">
-            <input className="input-form"
-              type="text"
-              name="emailid"
-              id="enterEmail"
-              placeholder="Enter Applicant Email Id"
-            />
-          </div>
-          <div className="form-group">
-            <input className="input-form"
-              type="text"
-              name="aadhaarno"
-              id="enterAadhaarNo"
-              placeholder="Enter Applicant Aadhaar No "
-              />
-          </div>
-          <div className="form-group">
-            <input className="input-form"
-              type="text" 
-              name="panno"
-              id="enterPanNo"
-              placeholder="Enter Applicant PAN no "
-              />
-          </div>
-          <div className="form-group">
-            <input className="input-form"
-              type="text"
-              name="salary"
-              id="enterSalary"
-              placeholder="Enter Applicant Salary"
-              />
-          </div>
-          <div className="form-group">
-            <input className="input-form"
-              type="text"  
-              name="loanamount"
-              id="enterAmount"
-              placeholder="Enter Loan Amount Required" 
-              />
-          </div>
-          <div className="form-group">
-            <input 
-              type="text"  className="input-form"
-              name="months"
-              id="enterMonths"
-              placeholder="Enter loan repayment months"
-              />
-          </div>
-          <div>
-            <input className="btn"
-            type="button" 
-            name="next"
-            id="next"
-            value="Next"  onClick={handleNextClick}
-            />
-          </div>
-        </form>
-      </div>
+    <div className="apply-form">
+      <form className="form1">
+        <div className="form-group">
+          <input className="input-form"
+            type="text"
+            name="Applicant Name"
+            id="enterName"
+            data-testid="enterName"
+            placeholder="Enter Applicant Name"
+          />
+        </div>
+        <div className="form-group">
+          <input className="input-form"
+            type="text"
+            name="address"
+            id="enterAddress"
+            data-testid="enterAddress"
+            placeholder="Enter Applicant Address"
+          />
+        </div>
+
+        <div className="form-group">
+          <input className="input-form"
+            type="text"
+            name="mobileNumber"
+            id="enterMobile"
+            data-testid="enterMobile"
+            placeholder="Enter Applicant Mobile"
+          />
+        </div>
+        <div className="form-group">
+          <input className="input-form"
+            type="text"
+            name="emailid"
+            id="enterEmail"
+            data-testid="enterEmail"
+            placeholder="Enter Applicant Email Id"
+          />
+        </div>
+        <div className="form-group">
+          <input className="input-form"
+            type="text"
+            name="aadhaarno"
+            id="enterAadhaarNo"
+            data-testid="enterAadhaarNo"
+            placeholder="Enter Applicant Aadhaar No "
+          />
+        </div>
+        <div className="form-group">
+          <input className="input-form"
+            type="text"
+            name="panno"
+            id="enterPanNo"
+            data-testid="enterPanNo"
+            placeholder="Enter Applicant PAN no "
+          />
+        </div>
+        <div className="form-group">
+          <input className="input-form"
+            type="text"
+            name="salary"
+            id="enterSalary"
+            data-testid="enterSalary"
+            placeholder="Enter Applicant Salary"
+          />
+        </div>
+        <div className="form-group">
+          <input className="input-form"
+            type="text"
+            name="loanamount"
+            id="enterAmount"
+            data-testid="enterAmount"
+            placeholder="Enter Loan Amount Required"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text" className="input-form"
+            name="months"
+            id="enterMonths"
+            data-testid="enterMonths"
+            placeholder="Enter loan repayment months"
+          />
+        </div>
+      </form>
+    </div>
   );
 }
 
