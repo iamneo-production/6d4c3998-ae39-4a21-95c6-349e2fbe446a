a84f80617592bb97bc9459c408c4a876f2251a97
Vamshi026 <vamshi.rajarapu@gmail.com>
2023-05-20 09:49:49 +0000

    profile and review pages added

--- reactapp/src/components/Customer/Profile/Profile.jsx
+++ reactapp/src/components/Customer/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect,useContext } from 'react'
 import { Link,useNavigate } from 'react-router-dom'
 import UserContext from "../../../context/UserContext"
-
+import './Profile.css'
 export default function Profile() {
         
     const { userModel,setUserModel } = useContext(UserContext);
@@ -22,13 +22,40 @@ export default function Profile() {
      <div>
       <h1> Customer Profile </h1>
       {userModel ? (
-        <div>
-          <p>User email: {userModel.email}</p>
-          <p>Your role: {userModel.userRole}</p>
-          {userModel.mobileNumber && (
-            <p>Mobile number: {userModel.mobileNumber}</p>
-          )}
-        </div>
+        <div className="profile-container">
+        <h2>Profile Information</h2>
+        <form className="profile-form">
+          <div className="form-field">
+            <label htmlFor="name">Name:</label>
+            <span id="name">{"customer.name"}</span>
+          </div>
+  
+          <div className="form-field">
+            <label htmlFor="mobile">Mobile Number:</label>
+            <span id="mobile">{"customer.mobile"}</span>
+          </div>
+  
+          <div className="form-field">
+            <label htmlFor="address">Address:</label>
+            <span id="address">{"customer.address"}</span>
+          </div>
+  
+          <div className="form-field">
+            <label htmlFor="email">Email:</label>
+            <span id="email">{"customer.email"}</span>
+          </div>
+  
+          <div className="form-field">
+            <label htmlFor="loanId">Loan ID:</label>
+            <span id="loanId">{"customer.loanid"}</span>
+          </div>
+  
+          <div className="form-field">
+            <label htmlFor="monthlyEmi">Monthly EMI:</label>
+            <span id="monthlyEmi">{"customer.monthlyemi"}</span>
+          </div>
+        </form>
+      </div>
       ) : (
         <p>Loading...</p>
       )}
