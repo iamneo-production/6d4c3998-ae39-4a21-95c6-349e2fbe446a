eb1eeb27dca1ef4ae25b1c0fa7e629b6e31e5be9
jillella-krishnakumar <jillellakrishnakumar@gmail.com>
2023-05-26 17:17:10 +0000

    jwt and loading state

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,64 +1,50 @@
-import React,{useState,useEffect} from "react"
-import UserContext from "./context/UserContext"
-import {Routes,Route} from "react-router-dom"
-import Login from "./components/Auth/Login/Login"
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Login from "./components/Auth/Login/Login";
 import Signup from "./components/Auth/Signup/Signup";
-
-import ApplyForm from "./components/Customer/ApplyForm/ApplyForm";
+import { UserProvider } from "./context/UserContext";
 import UserSharedLayout from "./sharedlayout/UserSharedLayout";
-import AdminSharedLayout from "./sharedlayout/AdminSharedLayout";
-import AdminHomePage from "./components/Admin/AdminHomePage/AdminHomePage"
-import ApprovalForm from "./components/Admin/ApprovalForm/ApprovalForm"
-import Profile from "./components/Customer/Profile/Profile";
-import LoanStatus from "./components/Customer/LoanStatus/LoanStatus";
-
-import './App.css';
-
+import AdminSharedlayout from "../src/sharedlayout/AdminSharedLayout";
+import { JwtTokenProvider } from "./context/TokenContext";
 
 function App() {
 
-  const [userModel, setUserModel] = useState(() => {
-    const storedUserModel = localStorage.getItem("userModel");
-
-    if (storedUserModel) {
-      return JSON.parse(storedUserModel);
-    }
-
-    return null;
-  });
-  useEffect(() => {
-    localStorage.setItem("userModel", JSON.stringify(userModel));
-  }, [userModel]);
-
   return (
-    <UserContext.Provider value={{ userModel, setUserModel }}>
+    <JwtTokenProvider>
 
-    
-    <div className="App">
-      <Routes> 
-         {/* PUBLIC ROUTE */} 
-         <Route path="/" element={<Login />} /> 
-         <Route path="/user/signup" element={<Signup />} /> 
-         <Route path="/user/login"  element= { <Login/>} /> 
-         <Route path="/admin/login"  element= { <Login/>} /> 
-                        
-                         {/* USER ROUTES */}
-               {/* This is kinda home page for user */}
-        <Route path="/user/home" element={<UserSharedLayout />} >
-          <Route index element={<ApplyForm/>}/>
-          <Route path="loanstatus" element={<LoanStatus/>}/>
-          <Route path="profile" element={<Profile/>}/>
-        </Route>
-  
-                         {/* ADMIN ROUTES */} 
-        <Route path="/admin/home" element={<AdminSharedLayout />} >
-          <Route index element={<AdminHomePage/>}/>
-          <Route path="approvalform" element={<ApprovalForm/>}/>
-        </Route >
-  
-     </Routes>
-    </div>
-  </UserContext.Provider>
+
+      <UserProvider>
+        <div className="App">
+          <ToastContainer />
+          <Routes>
+            {/* PUBLIC ROUTE */}
+            <Route path="/" element={<Login />} />
+            <Route path="/user/login" element={<Login />} />
+            <Route path="/user/signup" element={<Signup />} />
+            <Route path="/admin/login" element={<Login />} />
+
+            {/* USER ROUTES */}
+
+            <Route path="/user/home" element={<UserSharedLayout />}>
+              <Route index element={<ApplyForm />} />
+              <Route path="loanstatus" element={<LoanStatus />} />
+              <Route path="profile" element={<Profile />} />
+
+            </Route>
+
+
+            {/* ADMIN ROUTES */}
+
+            <Route path="/admin/home" element={<AdminSharedlayout />}>
+              <Route index element={<AdminHomePage />} />
+              <Route path="approvalform" element={<ApprovalForm />} />
+            </Route>
+          </Routes>
+        </div>
+
+      </UserProvider>
+    </JwtTokenProvider>
   );
 }
 
--- reactapp/src/components/Auth/Signup/Signup.jsx
+++ reactapp/src/components/Auth/Signup/Signup.jsx
@@ -1,179 +1,165 @@
 import React, {useState} from "react";
 import {Link } from "react-router-dom";
-import {signUpUser} from "../../../utils/userApi"
-import axios from "axios";
-import './Signup.css';
-
+import {signUpUser,loginUser} from "../../../utils/userApi"
+import  {toast} from 'react-toastify'
+import './Signup.css'
 
 export default function Signup() {
+  const toastMsg = (msg) => toast(msg);
+  const [userType, setAdminOrUser] = useState("");
+  
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("")
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false)
 
-  // const [userType, setAdminOrUser] = useState("");
-  
-  // const [email, setEmail] = useState("");
-  // const [userName, setUserName] = useState("");
-  // const [mobileNumber, setMobileNumber] = useState("")
-  // const [password, setPassword] = useState("");
-  // const [confirmPassword, setConfirmPassword] = useState("");
-  
-  const [userData,setUserData] = useState({
-    userType:"",
-    email:"",
-    userName:"",
-    mobileNumber:"",
-    password:"",
-    confirmPassword:""
-  })
-  const emailRegex= /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ ;
-  const passwordRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})");
+   const emailRegex= /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ ;
+  // /^[a-zA-Z0-9]{4,}$/
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/;
   const mobileNumberRegex = /^\d{10}$/;
 
 
-  function handleChange(e){
-    const {name,value} = e.target;
-    setUserData((prevData)=>{
-      return {
-        ...prevData,
-        [name] : value
-      }
-    })
-  }
-  async function handleSignup(){
-    if(userData.userType==="" ||  userData.email==="" || userData.userName==="" || userData.mobileNumber==="" || userData.password==="" || userData.confirmPassword===""){
-      alert("Please enter all fields")
+
+ async  function handleSignup(){
+    if(userType==="" ||  email==="" || userName==="" || mobileNumber==="" || password==="" || confirmPassword===""){
+      toastMsg("Please enter all fields")
       console.log("Please enter all details")
     }
-    else if(!emailRegex.test(userData.email)){
+    else if(!emailRegex.test(email)){
+      
       console.log("Invalid Email");
-      alert("Invalid Email");
+      toastMsg("Invalid Email");
       return;
     }
-    else if(!passwordRegex.test(userData.password)){
-      alert("Password must contaion atleast 6 characters, including one number, one lower and upper case character and one special charaacter like #,@,$,!")
+    else if(!passwordRegex.test(password)){  
+      toastMsg("Password must contaion atleast 8 characters, including one number, one lower and upper case character and one special charaacter like #,@,$,!")
       console.log("Password must contaion atleast 8 characters, including one number, one lower and upper case character and one special charaacter like #,@,$,!")
       return;
     }
-    else if(userData.password!==userData.confirmPassword){
-      alert("Passwords does not match")
+    else if(password!==confirmPassword){
+      toastMsg("Passwords does not match")
       return;
     }
-    else if(!mobileNumberRegex.test(userData.mobileNumber)){
+    else if(!mobileNumberRegex.test(mobileNumber)){
       console.log("Invalid mobile number");
-      alert("Invalid Mobile no.");
+      toastMsg("Invalid Mobile no.");
       return;
     }
     else{
 
-      const user = {
-        
-        "email":userData.email,
-        "mobileNumber":userData.mobileNumber,
-        "password":userData.password,
-        "userRole":userData.userType,
-        "username":userData.userName
-      };
-      console.log("user before sighnup",user);
-      const data = await signUpUser(userData.email,userData.mobileNumber,userData.password,userData.userType,userData.userName)
-      console.log("sts after signup completed",data)
-      
+      const response= await signUpUser(email,mobileNumber,password,userType,userName,setLoading)
+     
     }
   }
   return (
-    <div>
-      <div className="navbar-register">
-         Register 
-      </div>
-      <div className="register-form">
-        
-          <div>
-            <input
-              data-testid="userType"
-              className="input-style-signup" 
-              type="text"
-              name="userType"
-              id="user"
-              placeholder="Enter admin/user"
-              value={userData.userType}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <input
-            data-testid="email"
-            className="input-style-signup" 
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter email"
-              value={userData.email}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <input
-            data-testid="username"
-            className="input-style-signup" 
-              type="text"
-              name="userName"
-              id="username"
-              placeholder="Enter Username"
-              value={userData.userName}
-              onChange={handleChange}
-            />
-          </div>
-           <div>
-            <input
-            data-testid="mobileNumber"
-            className="input-style-signup" 
-              type="text"
-              name="mobileNumber"
-              id="mobileNumber"
-              placeholder="Enter Mobilenumber"
-              value={userData.mobileNumber}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <input
-            data-testid="password"
-            className="input-style-signup" 
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Password"
-              value={userData.password}
-              onChange={handleChange}
-              />
-          </div>
-          <div>
-            <input
-             data-testid="confirmPassword"
-             className="input-style-signup" 
-              type="password"
-              name="confirmPassword"
-              id="confirmPassword"
-              placeholder=" Confirm Password"
-              value={userData.confirmPassword}
-              onChange={handleChange}
-              />
-          </div>
-          <div>
-            <input
-             data-testid="submitButton"
-             className="signup-button" 
-              type="submit"
-              id="submitButton"
-              value="Submit"
-              onClick={()=>{
-                handleSignup()
-              }}
-              />
-          </div>
-          <p className="loginPara" >
-                  Aldready a user ?
-              <Link data-testid='signinLink' id="signinLink" to="/user/login">&nbsp; Login</Link>
+    <div className="register-container">
+    <div className="register-form">
+      <h1 className="navbar-register"> {loading?"Processing" : "Register"} </h1>
+  
+   
+        <input
+          data-testid="userType"
+          className="input-style-signup"
+          type="text"
+          name="user"
+          id="user"
+          placeholder="Enter admin/user"
+          value='user'
+          onChange={(e) => {
+            setAdminOrUser(e.target.value);
+          }}
+        />
+      
+        <input
+          data-testid="email"
+          className="input-style-signup"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
 
-          </p>
-        </div>
+   
+        <input
+          data-testid="username"
+          className="input-style-signup"
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Enter Username"
+          value={userName}
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+
+  
+        <input
+          data-testid="mobileNumber"
+          className="input-style-signup"
+          type="text"
+          name="mobileNumber"
+          id="mobileNumber"
+          placeholder="Enter Mobilenumber"
+          value={mobileNumber}
+          onChange={(e) => {
+            setMobileNumber(e.target.value);
+          }}
+        />
+   
+     
+        <input
+          data-testid="password"
+          className="input-style-signup"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+     
+      
+        <input
+          data-testid="confirmPassword"
+          className="input-style-signup"
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          placeholder=" Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => {
+            setConfirmPassword(e.target.value);
+          }}
+        />
+      
+      <div class="container-btn-para">
+  <input
+    data-testid="submitButton"
+    class="signup-button"
+    type="submit"
+    id="submitButton"
+    value="Submit"
+    onClick={() => {
+      handleSignup();
+    }}
+  />
+  <p class="loginPara">
+    Already a user? <Link data-testid="signinLink" id="signinLink" to="/user/login">&nbsp; Login</Link>
+  </p>
+</div>
+
+     
     </div>
+  </div>
+  
   );
 }
