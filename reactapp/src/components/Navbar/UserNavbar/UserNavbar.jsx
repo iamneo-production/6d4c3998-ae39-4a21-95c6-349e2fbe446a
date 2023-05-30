be220e906e366f50bbd671079103727f6cac8196
shivasaipadige <padigeshivasai@gmail.com>
2023-05-30 11:32:38 +0530

    changes in apply form and navbar

--- reactapp/src/components/Navbar/UserNavbar/UserNavbar.jsx
+++ reactapp/src/components/Navbar/UserNavbar/UserNavbar.jsx
@@ -1,12 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import './UserNavbar.css';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState,useContext } from 'react';
+import { Link,NavLink } from 'react-router-dom';
+import UserContext from '../../../context/UserContext';
 
 function Navigationbar() {
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState(1);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { userModel, setUserModel } = useContext(UserContext);
 
   const handleTabClick = (tabIndex) => {
     setActiveTab(tabIndex);
@@ -15,6 +17,12 @@ function Navigationbar() {
   const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+  function handleLogout() {
+    setUserModel(null);
+    localStorage.removeItem("jwtToken");
+    localStorage.removeItem("userModel");
+    navigate("/user/login");
+  }
 
   return (
     <>
@@ -26,13 +34,17 @@ function Navigationbar() {
           <div className="menu-line"></div>
         </div>
         <div className={`tabs ${isMenuOpen ? 'open' : ''}`}>
+
+      
           <ul>
             <li onClick={() => { navigate('/user/home'); handleTabClick(1); }} className={activeTab === 1 ? 'active' : ''}>Apply Loan</li>
             <li onClick={() => { navigate('/user/home/loanstatus'); handleTabClick(2); }} className={activeTab === 2 ? 'active' : ''}>Loan Status</li>
             <li onClick={() => { navigate('/user/home/profile'); handleTabClick(3); }} className={activeTab === 3 ? 'active' : ''}>Profile</li>
+          
           </ul>
+          
         </div>
-        <Link className='logout'>Log out</Link>
+        <div onClick={handleLogout} className='logout'>Log out</div>
       </div>
     </>
   );
