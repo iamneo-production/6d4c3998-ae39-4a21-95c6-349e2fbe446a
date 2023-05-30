40d8586b9a2531756ded137f7d706f1465ee4989
Achuth Dintakurthi <90602876+Achuth-AA@users.noreply.github.com>
2023-05-30 15:08:05 +0530

    Connecting the backend to the UI (Admin side)

--- reactapp/src/components/Admin/ApprovalForm/ApprovalForm.jsx
+++ reactapp/src/components/Admin/ApprovalForm/ApprovalForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AdminNavbar from "../../Navbar/AdminNavbar/AdminNavbar";
 import "./ApprovalForm.css";
 // import EditTwoToneIcon from "@mui/icons-material/EditTwoTone";
@@ -6,10 +6,14 @@ import "./ApprovalForm.css";
 import approvaldata from "../../../data/approvaldata.json";
 import {useNavigate,useParams} from "react-router-dom";
 
+import {BASE_URL} from "../../../utils/utils"
+
 
 function ApprovalForm() {
+
+  const token = localStorage.getItem('jwtToken');
   const navigate = useNavigate();
-  let approvalloans = approvaldata;
+  const [approvalLoans, setApprovalLoans] = React.useState([])
   const handleEdit = (id) => {
     navigate(`/admin/editStudent/${id}`)
   }
@@ -17,24 +21,69 @@ function ApprovalForm() {
   const handleDelete = (id) => {
     navigate(`/admin/deleteStudent/${id}`)
   }
+
+  // GET ALLL LOANS 
+  useEffect(()=>{
+   
+   async function getAllLoans(){
+      try{
+        const res = await fetch(`${BASE_URL}/admin/getAllLoans`,{
+          method:"GET",
+          headers:{
+            Authorization:`Bearer ${token}`
+          }
+        })
+        if(!res.ok) throw (Error("failed to get all loans"))
+        const data = await res.json();
+        setApprovalLoans(data);
+       
+      }catch(e){
+        console.log(e.message,e)
+      }
+    }
+
+    getAllLoans();
+
+
+  },[])
+
+  // APPROVE OR REJECT
+  async function sendLoanStatus(data){
+
+
+  const res = await fetch(`${BASE_URL}/admin/editLoan/${data.loanId}`,{
+    method:"PUT",
+    headers:{
+      'Content-Type':'application/json',
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      loanStatus : data.loanStatus
+    })
+
+  })
+  const resData = await res.json();
+  console.log(resData)
+  }
+
   return (
     <>
       <AdminNavbar />
       
-      {approvalloans.map((application) => (
-        <div key={application.applicantLoanID} className="adminLoanGrid1">
+      {approvalLoans.map((application) => (
+        <div key={application.loanId} className="adminLoanGrid1">
           <p>Name: {application.applicantName}</p>
-          <p>Phone no: {application.applicantPhoneNo}</p>
+          <p>Phone no: {application.applicantMobile}</p>
           <p>Email: {application.applicantEmail}</p>
-          <p>Aadhar: {application.applicantAadhar}</p>
-          <p>PAN: {application.applicantPanNo}</p>
+          <p>Aadhar: {application.applicantAadhaar}</p>
+          <p>PAN: {application.applicantPan}</p>
           <p>Salary: {application.applicantSalary}</p>
-          <p>Loan Amt: {application.applicantLoanamt}</p>
-          <p>Repayment Months: {application.applicantRepaymentMon}</p>
+          <p>Loan Amt: {application.loanAmountRequired}</p>
+          <p>Repayment Months: {application.loanRepaymentMonths}</p>
           <div className="button-container">
-            <button className="approve-button">Approve</button>
+            <button className="approve-button" onClick={()=>{sendLoanStatus({loanId:application.loanId,loanStatus:"approve"})}} >Approve</button>
             <div className="action-buttons">
-              <button className="reject-button">Reject</button>
+              <button className="reject-button" onClick={()=>{sendLoanStatus({loanId:application.loanId,loanStatus:"rejected"})}} >Reject</button>
               <div className="icon-container">
                 <button type="submit" id="editLoan" onClick={()=>handleEdit(application.applicantLoanID)}>
                 <i title="Edit Loan">
