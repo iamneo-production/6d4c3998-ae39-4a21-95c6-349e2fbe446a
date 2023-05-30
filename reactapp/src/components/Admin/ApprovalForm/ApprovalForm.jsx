5b781d44ce41c6114b704d1cc960d598213baaa4
Achuth Dintakurthi <90602876+Achuth-AA@users.noreply.github.com>
2023-05-30 23:31:03 +0530

    changes in approval form

--- reactapp/src/components/Admin/ApprovalForm/ApprovalForm.jsx
+++ reactapp/src/components/Admin/ApprovalForm/ApprovalForm.jsx
@@ -118,7 +118,8 @@ function ApprovalForm() {
           </div>
         ))
       ) : (
-        <p>No pending loans found.</p>
+        <div className="img-contain">
+        <img src="https://i.pinimg.com/originals/49/e5/8d/49e58d5922019b8ec4642a2e2b9291c2.png" className="image"  alt="img" /></div>
       )}
 
       <p data-testid="customerName"></p>
