0f1ca287667a726c5160d3a258da903304c4e3fc
jillella-krishnakumar <jillellakrishnakumar@gmail.com>
2023-06-28 07:13:23 +0000

    cmt by ad3

---
+++ reactapp/src/sharedlayout/AdminSharedLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+<<<<<<< HEAD
+import AdminNavbar from "../components/Navbar/AdminNavbar/AdminNavbar"
+=======
+>>>>>>> 97abf19 (user-admin-routes-protection)
+
+export default function AdminSharedLayout() {
+  return (
+    <div>
+<<<<<<< HEAD
+      <AdminNavbar/>
+=======
+      
+>>>>>>> 97abf19 (user-admin-routes-protection)
+      <Outlet/>
+    </div>
+  )
+}
