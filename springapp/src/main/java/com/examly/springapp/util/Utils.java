69e369319d6388eff6fb6e9eee7ab8e85067c26d
Achuth_dintakurthi <58482564+Achuth-aiml-cloud@users.noreply.github.com>
2023-07-27 16:50:10 +0530

    Delete springapp/src/main/java/com/examly/springapp/util directory

--- springapp/src/main/java/com/examly/springapp/util/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.util;
-
-public class Utils {
-    public static double calculateEMI(double loanAmount, int repaymentMonths, double interestRate) {
-        double monthlyInterestRate = interestRate / 12;
-        double denominator = Math.pow(1 + monthlyInterestRate, repaymentMonths) - 1;
-        return (loanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, repaymentMonths)) / denominator;
-    }
-}
